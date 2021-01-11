5217ec34a3aaa568e87c6c97a8600441d069ec24
efilin <2727fas@gmail.com>
2021-01-11 23:56:06 +0300

    119. Added specialities, added to vet.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by efilin on 31.12.2020.
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
