f848ee62807165d1ab29297236a2bf3aec1ffc7d
efilin <2727fas@gmail.com>
2021-01-15 16:09:41 +0300

    148.Updated visit with JPA mappings.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,14 +1,24 @@
 package guru.springframework.sfgpetclinic.model;
 
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by efilin on 11.01.2021.
  */
-public class Visit {
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
