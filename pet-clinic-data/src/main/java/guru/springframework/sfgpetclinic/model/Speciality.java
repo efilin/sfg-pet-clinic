7ed2f250ab05b6bcbefead6f8f987a2329a8176d
efilin <2727fas@gmail.com>
2021-01-15 15:25:07 +0300

    147.Updated vet with JPA mappings.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by efilin on 11.01.2021.
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
